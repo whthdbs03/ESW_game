f59f05fb15cf5afc30f084a6fb81c56d1bfee93c
whthdbs03 <whvod222@naver.com>
2023-12-06 06:00:46 +0900

    w7

---
+++ w7/attack.py
@@ -0,0 +1,34 @@
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+import time
+import random, os
+from colorsys import hsv_to_rgb
+class attack:
+    def __init__(self, position, command):
+        '''
+        position : 돌멩이 발사한 Character의 중앙 위치
+        command : 조이스틱 입력 값
+        '''
+        self.appearance = Image.open('image/attack.png')
+        self.speed = 20
+        self.damage = 10
+        self.position = np.array([position[0]-3, position[1]-3, position[0]+3, position[1]+3])
+        self.center = np.array([(self.position[0] + self.position[2]) / 2, (self.position[1] + self.position[3]) / 2])
+
+        self.direction = {'left' : False, 'right' : False}
+        self.state = None
+        self.outline = "#0000FF"
+        
+        if command['right_pressed']:
+            self.direction['right'] = True
+        if command['left_pressed']:
+            self.direction['left'] = True
+
+    def move(self):
+        if self.direction['left']:
+            self.position[0] -= self.speed
+            self.position[2] -= self.speed
+            
+        if self.direction['right']:
+            self.position[0] += self.speed
+            self.position[2] += self.speed
